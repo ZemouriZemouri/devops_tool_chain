b47795494d8bbb1da3bc8086ba2afd6671d5dbbe
otman <otman@qualityaccelerators.nl>
2025-05-08 20:57:17 +0200

    Refactoring

--- projects/jenkins-project/jenkins-plugin-model/ci/01-build.sh
+++ projects/jenkins-project/jenkins-plugin-model/ci/01-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH="/usr/share/dotnet:$PATH"
-dotnet build jenkins-plugin-model/src/Pi.Web/Pi.Web.csproj
+dotnet build projects/jenkins-project/jenkins-plugin-model/src/Pi.Web/Pi.Web.csproj
