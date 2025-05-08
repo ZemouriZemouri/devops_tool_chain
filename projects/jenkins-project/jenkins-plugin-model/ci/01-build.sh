deaa4e9730746d0e0c5715f27118aac3cc753cee
otman <otman@qualityaccelerators.nl>
2025-05-08 20:13:56 +0200

    dotnet path workaround

--- projects/jenkins-project/jenkins-plugin-model/ci/01-build.sh
+++ projects/jenkins-project/jenkins-plugin-model/ci/01-build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+export PATH="/usr/share/dotnet:$PATH"
 dotnet build jenkins-plugin-model/src/Pi.Web/Pi.Web.csproj
--- projects/jenkins-project/jenkins-plugin-model/ci/02-unit-test.sh
+++ projects/jenkins-project/jenkins-plugin-model/ci/02-unit-test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+export PATH="/usr/share/dotnet:$PATH"
 dotnet test --logger "trx;LogFileName=Pi.Math.trx" jenkins-plugin-model/src/Pi.Math.Tests/Pi.Math.Tests.csproj
+export PATH="/usr/share/dotnet:$PATH"
 dotnet test --logger "trx;LogFileName=Pi.Runtime.trx" jenkins-plugin-model/src/Pi.Runtime.Tests/Pi.Runtime.Tests.csproj
